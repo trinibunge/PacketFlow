97f7d7468d3863fd30beebe6b740c7b5d52bc784
Maximiliano López <maximiliano.lopez@correo.ucu.edu.uy>
2026-04-15 15:18:07 -0300

    agrego clase mensaje

---
+++ PacketFlow/src/main/java/Mensaje.java
@@ -0,0 +1,56 @@
+import java.util.Queue;
+import java.util.ArrayDeque;
+
+
+public class Mensaje {
+    private int idMensaje;
+    private Queue <Paquete> paquetes;
+    private int prioridad;
+
+    public Mensaje(int idMensaje, int prioridad) {
+        this.idMensaje = idMensaje;
+        this.prioridad = prioridad;
+        this.paquetes = new ArrayDeque<>();
+    }
+    // Getters
+    public int getIdMensaje() {
+        return idMensaje;
+    }
+    public int getPrioridad () {
+        return prioridad;
+    }
+    public Queue<Paquete> getPaquetes() {
+        return paquetes;
+    }
+
+    // Setters
+    public void setIdMensaje(int idMensaje) {
+        this.idMensaje = idMensaje;}
+    public void setPrioridad(int prioridad) {
+        this.prioridad = prioridad;
+    }
+
+    // metodo para separar el mensaje en paquetes
+    public void Fragmentar(String contenido, int tamaño){
+        // calculo paquetes necesarios para el mensaje, ceil redondea para arriba la division
+        int CantPaquetes = (int) Math.ceil((double) contenido.length() / tamaño);
+
+        for(int i = 0; i < CantPaquetes; i++){
+            int principio = i * tamaño;
+            int fin = Math.min(principio + tamaño, contenido.length());
+            String frag = contenido.substring(principio, fin);
+
+         Paquete paq = new Paquete(
+                 idMensaje,
+                 i + 1,
+                 CantPaquetes,
+                 frag,
+                 EstadoPaquete.EN_TRANSITO,
+                 this
+         );
+        paquetes.add(paq);
+        }
+
+    }
+
+}
